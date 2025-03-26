8f8b16883362ea5e08896b1859b63c6b4b356671
Lynx <lynx@wiltner.net>
2025-03-26 10:24:47 +0100

    settings update

--- src/Main.java
+++ src/Main.java
@@ -1,104 +1,54 @@
 public class Main {
     public static void main(String[] args) {
         /*
-         * System.out.println("Hello aaa");
-         * String name = "Lynx";
-         * System.out.println(name);
-         * int age = 22;
-         * System.out.println(age);
-         * double heightInMeters = 1.69;
-         * System.out.println(heightInMeters);
-         * boolean isStudent = true;
+         * System.out.println("Hello aaa"); String name = "Lynx"; System.out.println(name); int age
+         * = 22; System.out.println(age); double heightInMeters = 1.69;
+         * System.out.println(heightInMeters); boolean isStudent = true;
          * System.out.println(isStudent);
          * 
-         * int a = 8;
-         * int b = 3;
-         * int sum = a + b;
-         * System.out.println("Summe: " + sum);
-         * int diff = a - b;
-         * System.out.println(diff);
-         * int multi = a * b;
-         * System.out.println(multi);
-         * int div = a / b;
-         * System.out.println(div);
-         * int mod = a % b;
-         * System.out.println(mod);
+         * int a = 8; int b = 3; int sum = a + b; System.out.println("Summe: " + sum); int diff = a
+         * - b; System.out.println(diff); int multi = a * b; System.out.println(multi); int div = a
+         * / b; System.out.println(div); int mod = a % b; System.out.println(mod);
          * 
-         * boolean equals = a == b;
-         * System.out.println(equals);
-         * boolean greater = a > b;
-         * System.out.println(greater);
-         * boolean less = a < b;
-         * System.out.println(less);
+         * boolean equals = a == b; System.out.println(equals); boolean greater = a > b;
+         * System.out.println(greater); boolean less = a < b; System.out.println(less);
          * 
-         * double c = 1.32;
-         * double d = 4.04;
+         * double c = 1.32; double d = 4.04;
          * 
-         * double sum2 = c + d;
-         * System.out.println(sum2);
-         * double diff2 = c - d;
-         * System.out.println(diff2);
-         * double mul2 = c * d;
-         * System.out.println(mul2);
-         * double div2 = c / d;
-         * System.out.println(div2);
-         * double mod2 = c % d;
-         * System.out.println(mod2);
+         * double sum2 = c + d; System.out.println(sum2); double diff2 = c - d;
+         * System.out.println(diff2); double mul2 = c * d; System.out.println(mul2); double div2 = c
+         * / d; System.out.println(div2); double mod2 = c % d; System.out.println(mod2);
          * 
-         * boolean equals2 = c == d;
-         * System.out.println(equals2);
-         * boolean greater2 = c > d;
-         * System.out.println(greater2);
-         * boolean less2 = c < d;
-         * System.out.println(less2);
+         * boolean equals2 = c == d; System.out.println(equals2); boolean greater2 = c > d;
+         * System.out.println(greater2); boolean less2 = c < d; System.out.println(less2);
          * 
-         * String hello = "Hello";
-         * String world = "World";
-         * System.out.println(hello.equals(world));
+         * String hello = "Hello"; String world = "World"; System.out.println(hello.equals(world));
          * System.out.println(hello.compareTo(world));
          */
 
         /*
          * int age = 22;
          * 
-         * if (age > 18) {
-         * System.out.println("You are older than 18");
-         * }
+         * if (age > 18) { System.out.println("You are older than 18"); }
          * 
          * String name = "Lynx";
          * 
-         * if (name.length() > 10) {
-         * System.out.println("Your name is too long");
-         * }
+         * if (name.length() > 10) { System.out.println("Your name is too long"); }
          * 
          * int grade = 91;
          * 
-         * if (grade > 90) {
-         * System.out.println("Note A");
-         * } else if (grade > 70) {
-         * System.out.println("Note B");
-         * } else if (grade > 50) {
-         * System.out.println("Note C");
-         * } else if (grade > 20){
-         * System.out.println("Note D");
-         * } else {
-         * System.out.println("Note f");
-         * }
+         * if (grade > 90) { System.out.println("Note A"); } else if (grade > 70) {
+         * System.out.println("Note B"); } else if (grade > 50) { System.out.println("Note C"); }
+         * else if (grade > 20){ System.out.println("Note D"); } else {
+         * System.out.println("Note f"); }
          * 
-         * int x = -81;
-         * if (x > 0) {
-         * System.out.println("x ist positiv");
-         * } else {
-         * System.out.println("x ist negativ");
-         * }
+         * int x = -81; if (x > 0) { System.out.println("x ist positiv"); } else {
+         * System.out.println("x ist negativ"); }
          */
 
         /*
-         * greeting("Lynx");
-         * System.out.println(add(5, 8));
-         * System.out.println(doubleAdd(4.24, 75.32));
-         * System.out.println(substract(7,2));
-         * System.out.println(makePositive(9));
+         * greeting("Lynx"); System.out.println(add(5, 8)); System.out.println(doubleAdd(4.24,
+         * 75.32)); System.out.println(substract(7,2)); System.out.println(makePositive(9));
          */
 
         int a = 5;
